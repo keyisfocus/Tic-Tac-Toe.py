d2675fee2d234f7e0fe3e234dcaa1e2f2758c72d
keyisfocus <keyisfocus@gmail.com>
2021-03-27 21:59:43 +0300

    Stage 5

--- tictactoe.py
+++ tictactoe.py
@@ -6,11 +6,12 @@ def is_invalid(string_to_check):
     return re.match("^\\d\\s\\d$", string_to_check) is None
 
 
-line = input("Enter cells: ")
+line = "_________"
 matrix = [list(line[0:3]), list(line[3:6]), list(line[6:9])]
 
 tictactoe_utils.print_grid(matrix)
 
+x_move = True
 while True:
     user_input = input("Enter the coordinates: ")
     if is_invalid(user_input):
@@ -26,9 +27,11 @@ while True:
         print("This cell is occupied! Choose another one!")
         continue
 
-    matrix[coords[0] - 1][coords[1] - 1] = 'X'
-    break
+    matrix[coords[0] - 1][coords[1] - 1] = 'X' if x_move else 'O'
+    x_move = not x_move
+    tictactoe_utils.print_grid(matrix)
 
-tictactoe_utils.print_grid(matrix)
-
-# print(tictactoe_utils.analyze_state(matrix))
+    state = tictactoe_utils.analyze_state(matrix)
+    if state != "Game not finished":
+        print(state)
+        break
